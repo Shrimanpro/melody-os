774ba7cd908500b2d4101ffe96c174dc23e1a022
Shriman <shrimanpro.gamer@gmail.com>
2026-07-13 17:22:54 -0700

    refactor: changed up some of the code to start

--- melody_os/src/main.rs
+++ melody_os/src/main.rs
@@ -1,20 +1,14 @@
-#![no_std]   // Disable the standard library
-#![no_main]  // Disable the standard Rust entry point
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
-
-// This function is called on a panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    // For now, just hang the processor indefinitely.
-    // Later, you will map this to output to a UART serial console.
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-// The linker looks for '_start' as the default entry point.
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    // Melody OS takes control of the Pi hardware right here.
-    
-    loop {} // Keep the processor spinning
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
